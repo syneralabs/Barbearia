d72c96d632647b3df823117557342c596d6e264e
Antonio Murilo <antoniomurilog@gmail.com>
2026-02-23 23:05:53 -0300

    Menu adicionado
    foi adicionado o header junto do menu mas só tá funcional para desktop

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,25 @@ function App() {
   
 
   return (
-    <div className='min-h-full'>
-      <div className='bg-gray-800/50'>
-        <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
-          
-        </div>
-      </div>
+    <div className='min-h-full bg-gray-900'>
+      <header className='absolute inset-0 top-0 z-50'>
+        <nav className='flex items-center justify-between px-6 lg:px-6'>
+          <div className='flex lg:px-1'> 
+            <a href="" className='m-1.5 p-1.5'>
+              <span className="sr-only">Your Company</span>
+              <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="" className="h-8 w-auto" />
+            </a>
+          </div>
+          <div className='hidden lg:flex lg:gap-x-12'>
+            <a className='text-sm/6 font-semibold text-black' href="">Home</a>
+            <a className='text-sm/6 font-semibold text-black' href="">sobre</a>
+            <a className='text-sm/6 font-semibold text-black' href="">Cortes</a>
+            <a className='text-sm/6 font-semibold text-black' href="">Contato</a>
+          </div>
+        </nav> 
+        {/* Final do nav */}
+      
+      </header>
     </div>
    
   )
