3175425b6d15fdb7270fee318262de43afd61539
Antonio Murilo <antoniomurilog@gmail.com>
2026-02-23 21:16:24 -0300

    Primeira Versão

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='min-h-full'>
+      <div className='bg-gray-800/50'>
+        <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+          
+        </div>
+      </div>
+    </div>
+   
+  )
+}
+
+export default App
